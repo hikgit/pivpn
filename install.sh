4adc99a3f522b4e2111d18064b0d708cfc223fc2
James <hikaus+git@gmail.com>
2024-09-09 21:30:23 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-curl -L https://install.pivpn.io > install.sh
-curl https://raw.githubusercontent.com/hikgit/pivpn/master/options.conf > options.conf
-chmod +x install.sh
-./install.sh --unattended options.conf
+#!/bin/bash
+curl -L https://install.pivpn.io > /home/ubuntu/install.sh
+curl https://raw.githubusercontent.com/hikgit/pivpn/master/options.conf > /home/ubuntu/options.conf
+chmod +x /home/ubuntu/install.sh
+sudo -u ubuntu /home/ubuntu/install.sh --unattended options.conf
